cba9ea685c501105ca296dd98f06d1c1d3f5ba32
kattak2k <kkiran.ror@gmail.com>
2017-05-24 15:01:17 +0530

    candy model

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Things you may want to cover:
 * Ruby version
 
 * System dependencies
+Kids get a lot of different candy for halloween and candies go to a lot of different kids.
 
 * Configuration
 
---
+++ test/system/candies_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CandiesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit candies_url
+  #
+  #   assert_selector "h1", text: "Candy"
+  # end
+end
