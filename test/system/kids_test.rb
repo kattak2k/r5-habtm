026e3065303ddc92ecb5c564a7c87675a5b86c62
kattak2k <kkiran.ror@gmail.com>
2017-05-24 14:59:27 +0530

    initial commit & kids model

---
+++ test/system/kids_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class KidsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit kids_url
+  #
+  #   assert_selector "h1", text: "Kid"
+  # end
+end
